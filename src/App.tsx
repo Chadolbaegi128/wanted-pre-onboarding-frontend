ed90f065de2a84fcaaf945a98b7bddafb1c01ad0
elice31 <elice31@elice31ui-iMac.local>
2023-08-08 21:35:01 +0900

    feat: 프로젝트 라우팅 설정 및 랜딩 페이지, 회원가입 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/auth/SignUp';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>        
+      </BrowserRouter>
     </div>
   );
 }
