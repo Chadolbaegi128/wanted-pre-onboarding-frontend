22bdd7bd30e7215ec8706b427d7cadb44f393bf4
YeonjunJung <duswns12893@gmail.com>
2023-08-12 02:37:22 +0900

    Feat: 회원가입 페이지 생성

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container , Form, Input, InputWrapper, SignupTitle,
+    InputTitle, Button } from './signUp-styled';
 
 const SignUp = () => {
-    return (<>
-        SignUp
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+    <>
+        <Container>
+            <Form>
+                <SignupTitle>회원가입</SignupTitle>
+                <InputWrapper>
+                    <InputTitle>이메일</InputTitle>
+                    <Input 
+                        name='email'
+                        type='email'
+                        id='email'
+                        placeholder='example@email.com'
+                        required
+                    />
+                    <InputTitle>패스워드</InputTitle>
+                    <Input 
+                        name='password'
+                        type='password'
+                        id='password'
+                        placeholder='●●●●●●●●'
+                        required
+                    />
+                </InputWrapper>
+                <Button type='submit'>회원가입</Button>
+            </Form>
+        </Container>        
     </>)
 }
 
