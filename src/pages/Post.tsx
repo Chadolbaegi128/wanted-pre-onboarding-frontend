32d9bb14ec31d7bdab6c8d940c7228e0e4485017
YeonjunJung <duswns12893@gmail.com>
2023-08-16 23:43:18 +0900

    feat: 로그인 페이지 기능 수정

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -5,16 +5,13 @@ const Post = () => {
         <>
             <li>
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox" data-testid="new-todo-input"/>
                     <span>TODO 1</span>
                 </label>
             </li>
-            <li>
-                <label>
-                    <input type="checkbox" />
-                    <span>TODO 2</span>
-                </label>
-            </li>
+            <button data-testid="new-todo-add-button">추가</button>
+            <button data-testid="modify-button">수정</button>
+            <button data-testid="delete-button">삭제</button>
         </>
     )
 };
--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, ChangeEvent } from "react";
 import { Container , Form, Input, InputWrapper, SignupTitle,
     InputTitle, Button } from "./signin-styled";
 
@@ -6,8 +6,39 @@ const regEmail = /@/;
 const regPW = /.{8,}/;
 
 const SignIn = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    });
+
+    const [emailMessage, setEmailMessage] = useState<string>("");
+    const [passwordMessage, setPasswordMessage] = useState<string>("");
+
+    const [isEmail, setIsEmail] = useState<boolean>(false);
+    const [isPassword, setIsPassword] = useState<boolean>(false);
+
+    const handleEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setForm({...form, email: e.target.value});
+        if(!regEmail.test(e.target.value)){
+            setEmailMessage("이메일 형식이 틀렸습니다! 다시 작성해주세요~")
+            setIsEmail(false);
+        } else{
+            setEmailMessage("올바른 이메일 형식입니다!");
+            setIsEmail(true);
+        }
+    },[form]);
+
+
+    const handlePassword = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setForm({...form, password: e.target.value});
+        if(!regPW.test(e.target.value)){
+            setPasswordMessage("패스워드 형식이 틀렸습니다! 다시 작성해주세요~");
+            setIsPassword(false);
+        } else{
+            setPasswordMessage("올바른 패스워드 형식입니다!");
+            setIsPassword(true);
+        }
+    },[form])
 
     return (
     <>
@@ -20,16 +51,18 @@ const SignIn = () => {
                         name="email"
                         type="email"
                         data-testid="email-input"
-                        placeholder="example@email.com"
-                        required
+                        value={form.email}
+                        onChange={handleEmail}
+                        placeholder="example@email.com"                        
                     />
                     <InputTitle>패스워드</InputTitle>
                     <Input 
                         name="password"
                         type="password"
                         data-testid="password-input"
+                        value={form.password}
+                        onChange={handlePassword}
                         placeholder="●●●●●●●●"
-                        required
                     />
                 </InputWrapper>
                 <Button type="submit" data-testid="signin-button">Sign In</Button>
